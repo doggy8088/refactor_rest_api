566461294084517c04f1f2b5e9efee3f1529e689
Santhosh <santhosh83@gmail.com>
2020-03-31 22:28:57 +1100

    Adding Product Application Test

--- src/test/java/com/example/demo/ProductsApplicationTests.java
+++ src/test/java/com/example/demo/ProductsApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProductsApplicationTests {
 
+	@InjectMocks
+	ProductsApplication productsApplication;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void main() {
+		String[] arg = {"start"};
+		productsApplication.main(arg);
 	}
 
 }
